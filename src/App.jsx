67fbc19f76adc043e05a9d9df57a54a6854f16fb
oumaimabnz <oumaimabenaziza21@gmail.com>
2025-11-16 12:52:22 +0100

    feat: add ThemeProvider

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  return (
+    <ThemeProvider>
+      <div>
+        <Navbar/>
+      </div>
+    </ThemeProvider>
+    
+  )
+}
+
+export default App
