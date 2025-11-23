159d2c58070e8c6d7d379b13634ff010cc68e89a
oumaimabnz <oumaimabenaziza21@gmail.com>
2025-11-23 15:21:03 +0100

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SkillsSection from './components/Sections/SkillsSection'
 import ProjectsSection from './components/Sections/ProjectsSection'
 import AboutSection from './components/Sections/AboutSection'
 import ContactSection from './components/Sections/ContactSection'
+import Footer from './components/Sections/Footer'
 
 
 
@@ -13,14 +14,14 @@ const App = () => {
 
   return (
     <ThemeProvider>
-      <div className='pb-[100vh]'>
+      <div>
         <Navbar/>
         <HeroSection/>
         <SkillsSection/>
         <ProjectsSection/>
         <AboutSection/>
         <ContactSection/>
-        
+        <Footer/>
       </div>
     </ThemeProvider>
     
